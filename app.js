70cc379d618cb1e3fe42f00c1669c00558061523
beata <beata@emc2wave.com>
2020-01-20 15:48:47 -0800

    add classList.add('box')

--- app.js
+++ app.js
@@ -14,5 +14,6 @@ const container = document.querySelector("#boxes");
 for (let color of colors) {
   const box = document.createElement("div");
   box.style.backgroundColor = color;
+  box.classList.add("box");
   container.append(box);
 }
